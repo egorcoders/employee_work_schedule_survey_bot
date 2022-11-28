8c443a850451015f573331ab4f1d74f266668a27
Boev Egor <egorcoders@gmail.com>
2022-11-28 19:26:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ GET_UPDATES = 'https://api.telegram.org/bot5808451063:AAFoF9PAEcSsZnjT5im-Alr9sf
 TOKEN = '5808451063:AAFoF9PAEcSsZnjT5im-Alr9sfC0VyPPHq4'  # Токен бота
 CHAT_ID = '-653212702'  # Номер чата в котором работает бот
 POLL_DELAY = 60 * 60 * 24  # Ожидание до следующего опроса в секундах
-POLL_TIME = '09:00:00'  # Время проведения опроса
+POLL_TIME = '19:00:00'  # Время проведения опроса
